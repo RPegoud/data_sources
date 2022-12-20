767b92286748aacca33b488c6bf784e53e1d52df
RPegoud <ryan.pegoud@epfedu.fr>
2022-12-20 11:19:04 +0100

    working google analytics connection

--- my-micro/main.py
+++ my-micro/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, mak
 import requests
 
 app = Flask(__name__)
-
+g_analytics_url = 'https://analytics.google.com/analytics/web/#/p344199140/reports/intelligenthome'
 
 @app.route('/', methods=["GET"])
 def index():
@@ -14,12 +14,14 @@ def show_logs():
 
 @app.route('/cookie/', methods=["GET"])
 def cookie(): 
-    if not request.cookies.get('google'):
-        res = make_response("Setting a cookie")
-        req = str(requests.request(url="https://www.google.com/", method='GET').cookies.get_dict())
-        res.set_cookie('google', req, max_age=60*60*24*365*2)
-    else:
-        res = make_response(f"Value of cookie: {request.cookies.get('google')}")
-    return res
+      # create and print a cookie
+      # if not request.cookies.get('google_analytics'):
+      #     res = make_response("Setting a cookie")
+      #     # req = str(requests.request(url=g_analytics_url, method='GET'))
+      #     req = requests.get(g_analytics_url).text
+      #     res.set_cookie('google_analytics', req, max_age=60*60*24*365*2)
+      # else:
+      #     res = make_response(f"Value of cookie: {request.cookies.get('google_analytics')}")
+      # return res
+    return requests.get(g_analytics_url).text 
 
-     # https://analytics.google.com/analytics/web/#/p344199140/reports/intelligenthome
